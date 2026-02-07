e832354009f3da2dcdefa3e7bbadb968f6f5bcfa
Luchian <luchian-1999@mail.ru>
2026-02-07 20:31:06 +0300

    Added test for insertion sort

---
+++ test/test_insertion_sort.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append("../sortingAlgorithms")
+
+import random
+from typing import List
+
+import pytest
+
+from insertion_sort import insertion_sort
+
+
+
+def create_random_array(N:int, val_from:int = 100, val_to:int = 10_000) -> List:
+    """Given a number of items N generates a random list with values from val_from to val_to"""
+    return [random.randint(val_from, val_to) for _ in range(N)]
+
+check_lists = [(create_random_array(N), N) for N in range(1000)]
+@pytest.mark.parametrize("the_list, list_length", check_lists)
+def test_insertion_sort(the_list: List, list_length: int):
+    test_list = the_list.copy()
+    expected_list = the_list.copy()
+    
+    insertion_sort(test_list, list_length)
+    expected_list.sort()
+    
+    assert test_list == expected_list, f"Failed for list of length {list_length}"
